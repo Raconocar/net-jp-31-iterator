27f0d6f41cb25f7e6f3b6485ee10fbe7dc0770ad
Raconocarn <Stranger_forever_rus112@rambler.ru>
2022-05-26 22:51:00 +0300

    add support random class

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -3,14 +3,14 @@ import java.util.Iterator;
 import java.util.function.Consumer;
 
 public class Randoms implements Iterable<Integer> {
-    protected Random random;
-
+    myRandomeImpl myRand;
 
     private List<Integer> randomes;
 
     public Randoms(int min, int max) {
-        this.randomes= new ArrayList<>();
-        this.randomes.add((random.nextInt(max - min)) + min);
+        this.randomes = new ArrayList<>();
+        this.myRand = new myRandomeImpl(min, max);
+
     }
 
     @Override
@@ -27,7 +27,7 @@ public class Randoms implements Iterable<Integer> {
             @Override
             public Integer next() {
                 next++;
-                int actual = randomes.get(next);
+                int actual = myRand.getRand();
                 return actual;
             }
         };
---
+++ src/main/java/myRandomeImpl.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class myRandomeImpl {
+    protected static Random random;
+    protected static int min;
+    protected static int max;
+
+    public myRandomeImpl(int min, int max) {
+        this.max = max;
+        this.min = min;
+        this.random = new Random();
+    }
+
+    public static int getRand() {
+         int ranInt = random.nextInt(max - min) + min+1;
+        System.out.println(ranInt);
+
+        return ranInt;
+    }
+
+}
